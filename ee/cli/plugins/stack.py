9329e00819fc7209effe857e9620ce6fb7f0b7d5
Bharat Parsiya <bnap00@gmail.com>
2017-06-30 19:13:00 +0530

    install phpmyadmin with composer

--- ee/cli/plugins/stack.py
+++ ee/cli/plugins/stack.py
@@ -1680,6 +1680,9 @@ class EEStackController(CementBaseController):
                 EEExtract.extract(self, '/tmp/pma.tar.gz', '/tmp/')
                 Log.debug(self, 'Extracting file /tmp/pma.tar.gz to '
                           'location /tmp/')
+                if not EEShellExec.cmd_exec(self, "which composer"):
+                    EEDownload.download(self,"composer")
+                EEShellExec.cmd_exec(self,"cd /tmp/phpmyadmin-STABLE && composer install")
                 if not os.path.exists('{0}22222/htdocs/db'
                                       .format(EEVariables.ee_webroot)):
                     Log.debug(self, "Creating new  directory "
@@ -2194,6 +2197,9 @@ class EEStackController(CementBaseController):
                 self.app.pargs.adminer = True
                 self.app.pargs.phpmyadmin = True
                 self.app.pargs.utils = True
+            
+            if self.app.pargs.phpmyadmin:
+                self.app.pargs.composer = True
 
             if self.app.pargs.mail:
                 self.app.pargs.nginx = True
